abbf84555713a6cd554c46ac35db2e46b3e74fe9
Hoàng Minh Thắng <p@banphim.net>
2012-12-01 19:57:33 +0700

    set ver to 1.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject clansi "1.2.0-SNAPSHOT"
+(defproject myguidingstar/clansi "1.3.0"
   :description "ANSI style and color codes for Clojure."
-  :plugins [[lein-swank "1.4.4"]]
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
